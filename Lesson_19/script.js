fd16aceca624bfee62e6efc894b79146fb2269e2
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-11-07 15:29:44 +0200

    fox2

--- Lesson_19/script.js
+++ Lesson_19/script.js
@@ -3,11 +3,66 @@
 // https://swapi.dev/
 
 // Перерва до  21.00
+// const search = document.querySelector(".js-search");
+// const list = document.querySelector(".js-list");
+// search.addEventListener("submit", onSearch);
+
+// function onSearch(evt) {
+//   evt.preventDefault();
+
+//   const { query, days } = evt.currentTarget.elements;
+//   getWeather(query.value, days.value)
+//     .then((data) => (list.innerHTML = createMarkup(data.forecast.forecastday)))
+//     .catch((err) => console.log(err));
+// }
+
+// function getWeather(city, days) {
+//   const BASE_URL = "http://api.weatherapi.com/v1";
+//   const API_KEY = "ce2cb9b2a3da414bb5b172546231704";
+//   const params = new URLSearchParams({
+//     key: API_KEY,
+//     q: city,
+//     days: days,
+//     lang: "uk",
+//   });
+
+//   return fetch(`${BASE_URL}/forecast.json?${params}`).then((resp) => {
+//     if (!resp.ok) {
+//       throw new Error(resp.statusText);
+//     }
+
+//     return resp.json();
+//   });
+// }
+
+// function createMarkup(arr) {
+//   return arr
+//     .map(
+//       ({
+//         date,
+//         day: {
+//           avgtemp_c,
+//           condition: { text, icon },
+//         },
+//       }) => `<li>
+//     <img src="${icon}" alt="${text}">
+//     <p>${text}</p>
+//     <h2>${date}</h2>
+//     <h3>${avgtemp_c}</h3>
+// </li>`
+//     )
+//     .join("");
+// }
+
+// fetch(
+//   "https://common-api.rozetka.com.ua/v1/api/sellers?country=UA&lang=ru&ids=21666"
+// );
+
 const search = document.querySelector(".js-search");
 const list = document.querySelector(".js-list");
-search.addEventListener("submit", onSearch);
+search.addEventListener("submit", onSaerch);
 
-function onSearch(evt) {
+function onSaerch(evt) {
   evt.preventDefault();
 
   const { query, days } = evt.currentTarget.elements;
@@ -17,17 +72,12 @@ function onSearch(evt) {
 }
 
 function getWeather(city, days) {
+  //http://api.weatherapi.com/v1/forecast.json?key=2a8d1a66318246e9b1c81420240611&q=Paris&days=5
   const BASE_URL = "http://api.weatherapi.com/v1";
-  const API_KEY = "ce2cb9b2a3da414bb5b172546231704";
-  const params = new URLSearchParams({
-    key : API_KEY,
-    q: city,
-    days: days,
-    lang: 'uk'
-  });
+  const API_KEY = "2a8d1a66318246e9b1c81420240611";
 
   return fetch(
-    `${BASE_URL}/forecast.json?${params}`
+    `${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=${days}&lang=uk`
   ).then((resp) => {
     if (!resp.ok) {
       throw new Error(resp.statusText);
@@ -44,14 +94,14 @@ function createMarkup(arr) {
         date,
         day: {
           avgtemp_c,
-          condition: { text, icon },
+          condition: { icon, text },
         },
-      }) => `<li>
-    <img src="${icon}" alt="${text}">
-    <p>${text}</p>
-    <h2>${date}</h2>
-    <h3>${avgtemp_c}</h3>
-</li>`
+      }) => `      <li>
+        <img src="${icon}" alt="${text}" />
+        <p>${text}</p>
+        <h2>${date}</h2>
+        <h3>${avgtemp_c}</h3>
+      </li>`
     )
     .join("");
 }
